d1ec2950e1c0ab59495b130549f7d9b1e476e2c3
rebeckaw <rebecka.weegar@umu.se>
2024-01-26 08:47:21 +0100

    speeches

--- api_swedeb/api/dummy_data/dummy_speech.py
+++ api_swedeb/api/dummy_data/dummy_speech.py
@@ -13,24 +13,56 @@ def get_speech_by_id(id: str):
     )
 
 
+
 def get_speeches(commons: CommonQueryParams):
-    return SpeechesResult(
-        speech_list=[
-            SpeechesResultItem(
-                speaker_column="En talare",
-                year_column= commons.from_year if commons.from_year else "1960",
-                gender_column="M",
+
+    speech_list = []
+    
+    if commons.parties:
+        parties = commons.parties
+    else:
+        parties = ['Parti 1']
+
+
+    if commons.genders:
+        genders = commons.genders
+    else:
+        genders = ['M', 'K', '?']
+    years = []
+    if commons.from_year:
+        years.append(commons.from_year)
+    else:
+        years.append("1920")
+
+    if commons.to_year:
+        years.append(commons.to_year)
+    else:
+        years.append("2020")
+
+    id = 1
+    for party in parties:
+        for gender in genders:
+            for year in years:
+                sri = SpeechesResultItem( speaker_column=f"Talare {id}",
+                    year_column= year,
+                    gender_column= gender,
+                    source_column="www.riksdagen.se",
+                    speech_id_column=str(id),
+                    party_column=party)
+                speech_list.append(sri)
+                id += 1
+
+    if len(speech_list) == 0:
+        speech_list.append(  SpeechesResultItem(
+                speaker_column="Herr Ej vald Metadata",
+                year_column= commons.from_year if commons.from_year else "1920",
+                gender_column= 'M',
                 source_column="www.riksdagen.se",
                 speech_id_column="1",
                 party_column="S",
-            ),
-            SpeechesResultItem(
-                speaker_column="Talarnamn",
-                year_column="1970",
-                gender_column="M",
-                source_column="www.riksdagen.se",
-                speech_id_column="2",
-                party_column="M",
-            ),
-        ]
+            ))
+
+
+    return SpeechesResult(
+        speech_list=speech_list
     )
