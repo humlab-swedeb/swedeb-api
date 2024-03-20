2562ab4b0c50e5fdce3146a39dd2f4c8093c784a
roger-mahler <roger.mahler@hotmail.com>
2024-03-20 09:51:29 +0100

    Added decoder inject function

--- api_swedeb/api/utils/dependencies.py
+++ api_swedeb/api/utils/dependencies.py
@@ -3,6 +3,7 @@ import os
 import ccc
 from fastapi import Depends
 
+from api_swedeb.api import parlaclarin as md
 from api_swedeb.api.utils.corpus import Corpus
 from api_swedeb.api.utils.kwic_corpus import KwicCorpus
 
@@ -18,12 +19,33 @@ async def get_kwic_corpus():
     return KwicCorpus(".env_1960")
 
 
-def get_cwb_corpus_opts():
+def get_cwb_corpus_opts() -> dict[str, str | None]:
     return {
-        "registry_dir": os.environ.get("CWB_REGISTRY_DIR"),
-        "corpus_name": os.environ.get("CWB_CORPUS_NAME"),
+        "registry_dir": os.environ.get("KWIC_DIR"),
+        "corpus_name": os.environ.get("KWIC_CORPUS_NAME"),
+        "data_dir": (
+            os.getenv("KWIC_TEMP_DIR") or f"/tmp/ccc-{str(ccc.__version__)}-{os.environ.get('USER', 'swedeb')}"
+        ),
     }
 
 
 async def get_cwb_corpus(opts: dict = Depends(get_cwb_corpus_opts)) -> ccc.Corpus:
-    return ccc.Corpora(opts.get("registry_dir")).corpus(opts.get("corpus_name"))
+    return ccc.Corpora(registry_dir=opts.get("registry_dir")).corpus(
+        corpus_name=opts.get("corpus_name"), data_dir=opts.get("data_dir")
+    )
+
+
+def get_decoder_opts() -> dict[str, str | None]:
+    return {
+        "metadata_filename": os.getenv("METADATA_FILENAME"),
+    }
+
+
+_corpus_codecs: md.Codecs = None
+
+
+async def get_corpus_decoder(opts: dict = Depends(get_decoder_opts)) -> ccc.Corpus:
+    global _corpus_codecs
+    if _corpus_codecs is None:
+        _corpus_codecs = md.PersonCodecs().load(source=opts.get("metadata_filename"))
+    return _corpus_codecs
