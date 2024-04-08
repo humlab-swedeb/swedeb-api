291db9acce60c969d7f49dccd901bf5a23393556
roger-mahler <roger.mahler@hotmail.com>
2024-04-08 09:24:00 +0200

    Add Docker files and configurations for the project

---
+++ docker/main.py
@@ -0,0 +1,22 @@
+import os
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from api_swedeb.api import metadata_router, tool_router
+
+
+app = FastAPI()
+
+origins = [f"http://localhost:{os.environ.get('SWEDEB_PORT', '8080')}"]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["GET", "POST"],
+    allow_headers=[],
+    allow_credentials=True,
+)
+
+app.include_router(tool_router.router)
+app.include_router(metadata_router.router)
