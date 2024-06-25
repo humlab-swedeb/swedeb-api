1c63fea1c5eab631b05917ba9b36eb5c9f1bee64
roger-mahler <roger.mahler@hotmail.com>
2024-06-25 15:40:27 +0200

    refactor: Remove unused code in get_ngrams function

--- api_swedeb/api/utils/ngrams.py
+++ api_swedeb/api/utils/ngrams.py
@@ -18,7 +18,6 @@ def get_ngrams(
     mode: Literal["dataframe", "counter"] = "dataframe",
 ) -> schemas.NGramResult:
 
-    # attribs: cwb.CorpusAttribs = n_grams.cwb(corpus)
     opts: dict[str, Any] = mappers.query_params_to_CQP_opts(commons, [(search_term, search_target)])
     ngrams: pd.DataFrame = n_grams.compute_n_grams(
         corpus, opts, n=n_gram_width, p_show=display_target, threshold=n_threshold, mode=mode
