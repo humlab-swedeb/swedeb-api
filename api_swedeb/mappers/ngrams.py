50a8d58f277199f80aa73a5791b61947c16acf15
roger-mahler <roger.mahler@hotmail.com>
2024-09-26 12:51:15 +0200

    feat: enhance ngrams_to_ngram_result to support custom separator and document splitting

--- api_swedeb/mappers/ngrams.py
+++ api_swedeb/mappers/ngrams.py
@@ -2,11 +2,11 @@ import pandas as pd
 from api_swedeb import schemas
 
 
-def ngrams_to_ngram_result(ngrams: pd.DataFrame) -> schemas.NGramResult:
+def ngrams_to_ngram_result(ngrams: pd.DataFrame, sep=" ") -> schemas.NGramResult:
     """Convert ngrams ndataframe witg columns ngram and count inti sequence of NGramResultItems"""
     return schemas.NGramResult(
         ngram_list=[
-            schemas.NGramResultItem(ngram="".join(x.ngram), count=x.count)
+            schemas.NGramResultItem(ngram=sep.join(x.ngram), count=x.count, documents=x.documents.split(","))
             for x in ngrams.itertuples(index=False)
         ]
     )
