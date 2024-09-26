135328540f5ce3790f8934bdeb15527490744d5e
roger-mahler <roger.mahler@hotmail.com>
2024-09-26 12:48:23 +0200

    tests: Add unit test for PersonCodecs loading functionality

---
+++ tests/test_codex.py
@@ -0,0 +1,11 @@
+
+from api_swedeb.api.parlaclarin.codecs import PersonCodecs
+
+from .config import METADATA_FILENAME
+
+def test_person_codecs():
+    filename: str = METADATA_FILENAME
+    person_codecs: PersonCodecs = PersonCodecs().load(source=filename)
+
+    assert person_codecs is not None
+
