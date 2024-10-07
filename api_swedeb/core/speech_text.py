76cb2f04e4b826d6dbec624d526420bd8ee56cbf
roger-mahler <roger.mahler@hotmail.com>
2024-10-07 20:05:07 +0200

    refactorings: move i/o methods to load.py refactoring: extract get_key_index method

--- api_swedeb/core/speech_text.py
+++ api_swedeb/core/speech_text.py
@@ -1,19 +1,15 @@
 from __future__ import annotations
 
-import abc
-import json
-import os
 import sqlite3
-import zipfile
 from functools import cached_property
-from os.path import join as jj
-from typing import Iterable, Literal
+from typing import Literal
 
 import numpy as np
 import pandas as pd
 from loguru import logger
 
 from . import codecs as md
+from .load import Loader, ZipLoader
 from .utility import fix_whitespace, read_sql_table
 
 # pylint: disable=unused-argument
@@ -32,7 +28,7 @@ class SpeechTextService:
 
     @cached_property
     def name2info(self) -> dict[str, dict]:
-        """Create a map protcol name to list of dict of relevant properties"""
+        """Create a map from protocol name to list of dict of relevant properties"""
         si: pd.DataFrame = self.speech_index.set_index("protocol_name", drop=True)[
             ["speech_id", "speech_index", self.id_name, "n_utterances"]
         ]
@@ -77,43 +73,6 @@ class SpeechTextService:
         )
 
 
-class Loader(abc.ABC):
-    @abc.abstractmethod
-    def load(self, protocol_name: str) -> tuple[dict, list[dict]]:
-        ...
-
-
-def zero_fill_filename_sequence(name: str) -> str:
-    parts: list[str] = name.split('-')
-    if parts[-1].isdigit():
-        parts[-1] = parts[-1].zfill(3)
-    return '-'.join(parts)
-
-
-class ZipLoader(Loader):
-    def __init__(self, folder: str):
-        self.folder: str = folder
-
-    def load(self, protocol_name: str) -> tuple[dict, list[dict]]:
-        """Loads tagged protocol data from archive"""
-        sub_folder: str = protocol_name.split("-")[1]
-        for filename in [
-            jj(self.folder, sub_folder, f"{protocol_name}.zip"),
-            jj(self.folder, f"{protocol_name}.zip"),
-        ]:
-            if not os.path.isfile(filename):
-                continue
-            with zipfile.ZipFile(filename, "r") as fp:
-                json_str: str = fp.read(f"{protocol_name}.json")
-                metadata_str: str = fp.read("metadata.json")
-            metadata: dict = json.loads(metadata_str)
-            # FIXME: This is a hack to fix the filename sequence number bug, later versions of the corpus should have this fixed
-            metadata['name'] = zero_fill_filename_sequence(metadata.get("name"))
-            utterances: list[dict] = json.loads(json_str)
-            return metadata, utterances
-        raise FileNotFoundError(protocol_name)
-
-
 class SpeechTextRepository:
     def __init__(
         self,
@@ -136,28 +95,21 @@ class SpeechTextRepository:
     def speech_id2id(self) -> dict[str, int]:
         return self.document_index.reset_index().set_index("speech_id")["document_id"].to_dict()
 
-    def load_protocol(self, protocol_name: str) -> tuple[dict, list[dict]]:
-        return self.source.load(protocol_name)
+    # def load_protocol(self, protocol_name: str) -> tuple[dict, list[dict]]:
+    #     return self.source.load(protocol_name)
 
-    def speeches(self, protocol_name: str) -> Iterable[dict]:
-        metadata, utterances = self.source.load(protocol_name)
-        return self.service.speeches(utterances=utterances, metadata=metadata)
+    # def speeches(self, protocol_name: str) -> Iterable[dict]:
+    #     metadata, utterances = self.source.load(protocol_name)
+    #     return self.service.speeches(utterances=utterances, metadata=metadata)
 
-    def _get_speech_info(self, key: int | str) -> dict:
+    def get_speech_info(self, key: int | str) -> dict:
         """Get speaker-info from document index and person table
         Accepts integer (document_id), speech_id (u_id of first utterance) and document_name ('prot-*)
         """
         if not isinstance(key, (int, str)):
             raise ValueError("key must be int or str")
 
-        if isinstance(key, int) or key.isdigit():
-            key_idx: int = int(key)
-        elif key.startswith('prot-'):
-            key_idx = self.document_name2id.get(key)
-        elif key.startswith('i-'):
-            key_idx = self.speech_id2id.get(key)
-        else:
-            raise ValueError(f"unknown speech key {key}")
+        key_idx: int = self.get_key_index(key)
 
         try:
             speech_info: dict = self.document_index.loc[key_idx].to_dict()
@@ -177,6 +129,17 @@ class SpeechTextRepository:
 
         return speech_info
 
+    def get_key_index(self, key):
+        if isinstance(key, int) or key.isdigit():
+            key_idx: int = int(key)
+        elif key.startswith('prot-'):
+            key_idx = self.document_name2id.get(key)
+        elif key.startswith('i-'):
+            key_idx = self.speech_id2id.get(key)
+        else:
+            raise ValueError(f"unknown speech key {key}")
+        return key_idx
+
     @cached_property
     def speaker_note_id2note(self) -> dict:
         try:
@@ -190,7 +153,7 @@ class SpeechTextRepository:
             logger.error(f"unable to read speaker_notes: {ex}")
             return {}
 
-    def speech(self, speech_name: str, mode: Literal["dict", "text", "html"]) -> dict | str:
+    def speech(self, speech_name: str, mode: Literal["dict", "text"]) -> dict | str:
         try:
             """Load speech data from speech corpus"""
             protocol_name: str = speech_name.split("_")[0]
@@ -199,7 +162,7 @@ class SpeechTextRepository:
             metadata, utterances = self.source.load(protocol_name)
             speech: dict = self.service.nth(metadata=metadata, utterances=utterances, n=speech_nr - 1)
 
-            speech_info: dict = self._get_speech_info(speech_name)
+            speech_info: dict = self.get_speech_info(speech_name)
             speech.update(**speech_info)
             speech.update(protocol_name=protocol_name)
 
