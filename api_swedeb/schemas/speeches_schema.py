6095f431ba0f4d73eb04a92b964ed657ab05019e
roger-mahler <roger.mahler@hotmail.com>
2024-10-07 13:43:40 +0200

    breaking change: updated /speeches schema

--- api_swedeb/schemas/speeches_schema.py
+++ api_swedeb/schemas/speeches_schema.py
@@ -6,13 +6,17 @@ from pydantic import BaseModel, Field
 class SpeechesResultItem(BaseModel):
     name: str = Field(None, description="Name of speaker")
     year: int = Field(None, description="Year of speech", examples=[1960])
-    gender: str = Field(None, description="Gender of speeker")
+    gender: str = Field(None, description="Gender of speaker")
+    gender_abbrev: str = Field(None, description="Gender of speaker")
     party_abbrev: str = Field(None, description="Party of speaker")
     speech_link: str = Field(None, description="Source of speech")
     document_name: str = Field(None, description="Unique id of speech")
     link: str = Field(None, description="Link to the speaker")
-    formatted_speech_id: str = Field(None, description="Formatted speech id")
-
+    speech_name: str = Field(None, description="Formatted speech id")
+    chamber_abbrev: str = Field(None, description="Chamber of speech")
+    speech_id: str = Field(None, description="Unique id of speech")
+    wiki_id: str = Field(None, description="Wiki id of speaker")
+    document_id: int = Field(None, description="Document system id")
 
 class SpeechesResult(BaseModel):
     speech_list: List[SpeechesResultItem]
