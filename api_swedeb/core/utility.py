d0b1dec513196dda3155a363b381edfeac3f6885
roger-mahler <roger.mahler@hotmail.com>
2024-10-10 20:59:22 +0200

    linting: silenced again

--- api_swedeb/core/utility.py
+++ api_swedeb/core/utility.py
@@ -347,14 +347,14 @@ def format_protocol_id(selected_protocol: str) -> str:
             # if len(protocol_parts) == 7:
             # prot-1958-a-ak--17-01_094
             return f"{chamber} {protocol_parts[1]}:{protocol_parts[5]} {id_parts}"
-        else:
-            #'prot-2004--113_075' -> '2004:113 075'
-            year = protocol_parts[1]
-            if len(year) == 4:
-                return f"{year[:4]}:{protocol_parts[3].replace('_', ' ')}"
-            #'prot-200405--113_075' -> '2004/05:113 075'
 
-            return f"{year[:4]}/{year[4:]}:{protocol_parts[3].replace('_', ' ')}"
+        #'prot-2004--113_075' -> '2004:113 075'
+        year = protocol_parts[1]
+        if len(year) == 4:
+            return f"{year[:4]}:{protocol_parts[3].replace('_', ' ')}"
+        #'prot-200405--113_075' -> '2004/05:113 075'
+
+        return f"{year[:4]}/{year[4:]}:{protocol_parts[3].replace('_', ' ')}"
     except IndexError:
         return selected_protocol
 
