0fc7c6d271dd798273f4b9b762d5b28e5b4d73f0
roger-mahler <roger.mahler@hotmail.com>
2024-10-22 15:48:33 +0200

    chore: fixed tests (change party ids)

--- tests/test_speakers.py
+++ tests/test_speakers.py
@@ -1,9 +1,11 @@
+from httpx import Response
 import pandas as pd
 import pytest
 from fastapi import status
 from fastapi.testclient import TestClient
 
 from api_swedeb.api.utils.corpus import Corpus, load_corpus
+from api_swedeb.core.codecs import PersonCodecs
 
 # pylint: disable=redefined-outer-name
 
@@ -28,19 +30,21 @@ def test_get_speakers(corpus):
     assert len(speakers) > 0
 
 
-def test_get_filtered_speakers_by_party_int(corpus):
+def test_get_filtered_speakers_by_party_int(person_codecs: PersonCodecs, corpus: Corpus):
     # party_id 9 is S
-    speakers = corpus.get_speakers(selections={'party_id': [9]})
+    sossarna_id: int = person_codecs.party_abbrev2id.get("S")
+
+    speakers: pd.DataFrame = corpus.get_speakers(selections={'party_id': [sossarna_id]})
     assert len(speakers) > 0
     assert 'S' in speakers['party_abbrev'].unique()
     assert all('S' in pa for pa in speakers['party_abbrev'].unique())
 
 
-def test_get_speakers_api(client):
+def test_get_speakers_api(client: TestClient):
     response = client.get(f"{version}/metadata/speakers")
     assert response.status_code == status.HTTP_200_OK
 
-    json = response.json()
+    json: dict = response.json()
     assert 'speaker_list' in json
     first_result = json['speaker_list'][0]
     assert 'name' in first_result
@@ -49,8 +53,9 @@ def test_get_speakers_api(client):
     assert 'year_of_death' in first_result
 
 
-def test_get_speakers_api_with_params(client):
-    response = client.get(f"{version}/metadata/speakers?party_id=9&gender_id=2")
+def test_get_speakers_api_with_params(person_codecs: PersonCodecs, client: TestClient):
+    sossarna_id: int = person_codecs.party_abbrev2id.get("S")
+    response: Response = client.get(f"{version}/metadata/speakers?party_id={sossarna_id}&gender_id=2")
     assert response.status_code == status.HTTP_200_OK
 
     json = response.json()
