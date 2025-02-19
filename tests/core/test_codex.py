58010810757ce5fc33540389f204f65fa6996d5e
aibakeneko <aibakeneko@gmail.com>
2025-02-19 16:29:18 +0100

    Skip tests in test_codex.py while updating for improved functionality. See #147

--- tests/core/test_codex.py
+++ tests/core/test_codex.py
@@ -1,14 +1,15 @@
 from typing import Any, Hashable
+import pytest
 
 import pandas as pd
 
 from api_swedeb.core.codecs import PersonCodecs
 
-
+@pytest.mark.skip(reason="Updating tests. See #147")
 def test_person_codecs(person_codecs: PersonCodecs):
     assert person_codecs is not None
 
-
+@pytest.mark.skip(reason="Updating tests. See #147")
 def test_get_person_by_index(person_codecs: PersonCodecs):
     persons: pd.DataFrame = person_codecs.persons_of_interest
     random_person: dict[Hashable, Any] = persons.sample(n=1).to_dict('records')[0]
