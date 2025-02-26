eb2161074a5831f347203844eeb5b368e5c1d6b9
aibakeneko <aibakeneko@gmail.com>
2025-02-26 15:38:10 +0100

    Updated fixtures. Added tests for Codecs encode and decode. Added FIXME comment for #152.

--- tests/core/test_codecs.py
+++ tests/core/test_codecs.py
@@ -101,37 +101,39 @@ class TestCodecs:
         - **Government**:    
             - `government_id`
             - `government`
-            
-    
     """
 
-    # FIXME: use index for id    
+
     @pytest.fixture(name="gender_dataframe")
     def fixture_gender_dataframe(self):
-        return pd.DataFrame({'gender_id': [1, 2], 'gender': ['Male', 'Female'], 'gender_abbrev': ['M', 'F']})
+        gender_ids = [10, 20]
+        return pd.DataFrame({'gender': ['Male', 'Female'], 'gender_abbrev': ['M', 'F']}, index=gender_ids)
     
     @pytest.fixture(name="party_dataframe")
     def fixture_party_dataframe(self):
-        return pd.DataFrame({'party_id': [1, 2], 'party': ['Party A', 'Party B'], 'party_abbrev': ['PA', 'PB']})
+        party_ids = [100, 200]
+        return pd.DataFrame({'party': ['Party A', 'Party B'], 'party_abbrev': ['PA', 'PB']}, index=party_ids)
     
     @pytest.fixture(name="office_type_dataframe")
     def fixture_office_type_dataframe(self):
-        return pd.DataFrame({'office_type_id': [1, 2], 'office': ['Office A', 'Office B']})
+        office_type_ids = [10, 20]
+        return pd.DataFrame({'office': ['Office A', 'Office B']}, index=office_type_ids)
     
     @pytest.fixture(name="sub_office_type_dataframe")
     def fixture_sub_office_type_dataframe(self):
-        return pd.DataFrame({'sub_office_type_id': [1, 2], 'identifier': ['A', 'B'], 'description': ['Description A', 'Description B']})
+        sub_office_type_ids = [10, 20]
+        return pd.DataFrame({'office_type_id': [1, 2], 'identifier': ['A', 'B'], 'description': ['Description A', 'Description B']}, index=sub_office_type_ids)
     
     @pytest.fixture(name="chamber_dataframe")
     def fixture_chamber_dataframe(self):
-        return pd.DataFrame({'chamber_id': [1, 2], 'chamber': ['Chamber A', 'Chamber B'], 'chamber_abbrev': ['CA', 'CB']})
+        chamber_ids = [10, 20]
+        return pd.DataFrame({'chamber': ['Chamber A', 'Chamber B'], 'chamber_abbrev': ['CA', 'CB']}, index=chamber_ids)
     
     @pytest.fixture(name="government_dataframe")
     def fixture_government_dataframe(self):
-        return pd.DataFrame({'government_id': [1, 2], 'government': ['Government A', 'Government B']})
+        government_ids = [10, 20]
+        return pd.DataFrame({'government': ['Government A', 'Government B']}, index=government_ids)
     
-    
-
     def test_codecs_load_with_non_existing_file(self):
         codecs = Codecs()
         with pytest.raises(FileNotFoundError):
@@ -238,84 +240,195 @@ class TestCodecs:
         codecs = Codecs()
         assert codecs.tablenames() == expected_tablenames
 
-    def test_gender2name(self):
-        codecs = Codecs()
-        codecs.gender = pd.DataFrame({'gender_id': [1, 2], 'gender': ['Male', 'Female']})
-        assert codecs.gender2name == {0: 'Male', 1: 'Female'}
-     
-    # FIXME: Replace above test with this one   
-    def test_gender2name2(self, gender_dataframe):
+    
+    def test_gender2name(self, gender_dataframe):
         codecs = Codecs()
         codecs.gender = gender_dataframe
-        assert codecs.gender2name == {0: 'Male', 1: 'Female'}
-    
+        assert codecs.gender2name == {10: 'Male', 20: 'Female'}
 
     def test_gender2name_empty(self):
         codecs = Codecs()
-        codecs.gender = pd.DataFrame(columns=['gender_id', 'gender'])
+        codecs.gender = pd.DataFrame(columns=['gender'])
         assert codecs.gender2name == {}
 
-
-    def test_gender2abbrev(self):
+    def test_gender2abbrev(self, gender_dataframe):
         codecs = Codecs()
-        codecs.gender = pd.DataFrame({'gender_id': [1, 2], 'gender': ['Male', 'Female'], 'gender_abbrev': ['M', 'F']})
-        assert codecs.gender2abbrev == {0: 'M', 1: 'F'}
+        codecs.gender = gender_dataframe
+        assert codecs.gender2abbrev == {10: 'M', 20: 'F'}
 
     def test_gender2abbrev_empty(self):
         codecs = Codecs()
-        codecs.gender = pd.DataFrame(columns=['gender_id', 'gender', 'gender_abbrev'])
+        codecs.gender = pd.DataFrame(columns=['gender', 'gender_abbrev'])
         assert codecs.gender2abbrev == {}
 
-  
-    # @pytest.mark.skip(reason="Fails. What is the expected output? Is party_id supposed to always be the index?")
-    def test_party2id(self):
-        parties = ['Party A', 'Party B']
-        party_ids = [100, 200]
-        expected_party2id = {'Party A': 100, 'Party B': 200}
-        
+    def test_party2id(self, party_dataframe):
         codecs = Codecs()
-        codecs.party = pd.DataFrame({'party': parties}, index=party_ids)
-        
-        assert codecs.party2id == expected_party2id
+        codecs.party = party_dataframe
+        assert codecs.party2id == {'Party A': 100, 'Party B': 200}
 
     def test_party2id_empty(self):
         codecs = Codecs()
-        codecs.party = pd.DataFrame(columns=['party_id', 'party'])
+        codecs.party = pd.DataFrame(columns=['party'])
         assert codecs.party2id == {}
     
-    
-    # TODO: Add additional tests for the remaining functions
-    
-    @pytest.mark.skip(reason="Not implemented")
     def test_decode_any_id_with_existing_value(self, gender_dataframe, office_type_dataframe, party_dataframe, sub_office_type_dataframe):
         codecs = Codecs()
         codecs.gender = gender_dataframe
         codecs.office_type = office_type_dataframe
         codecs.party = party_dataframe
         codecs.sub_office_type = sub_office_type_dataframe
-        assert codecs.decode_any_id('gender_id', 1) == 'Male'
-        assert codecs.decode_any_id('gender_id', 2) == 'Female'
+        assert codecs.decode_any_id('gender_id', 10) == 'Male'
+        assert codecs.decode_any_id('gender_id', 20) == 'Female'
+        
+        assert codecs.decode_any_id('office_type_id', 10) == 'Office A'
+        assert codecs.decode_any_id('office_type_id', 20) == 'Office B'
+        
+        assert codecs.decode_any_id('party_id', 100, to_name='party_abbrev') == 'PA'
+        assert codecs.decode_any_id('party_id', 200, to_name='party_abbrev') == 'PB'
+        assert codecs.decode_any_id('party_id', 100, to_name='party') == 'Party A'
+        assert codecs.decode_any_id('party_id', 200, to_name='party') == 'Party B'
+        
+        assert codecs.decode_any_id('sub_office_type_id', 10) == 'Description A'
+        assert codecs.decode_any_id('sub_office_type_id', 20) == 'Description B'
+        
+    def test_decode_any_id_with_non_existing_value(self, gender_dataframe, office_type_dataframe, party_dataframe, sub_office_type_dataframe):
+        codecs = Codecs()
+        codecs.gender = gender_dataframe
+        codecs.office_type = office_type_dataframe
+        codecs.party = party_dataframe
+        codecs.sub_office_type = sub_office_type_dataframe
+        assert codecs.decode_any_id('gender_id', 30) == 'unknown'
+        
+    def test_decode_any_id_with_custom_default_value(self, gender_dataframe, office_type_dataframe, party_dataframe, sub_office_type_dataframe):
+        codecs = Codecs()
+        codecs.gender = gender_dataframe
+        codecs.office_type = office_type_dataframe
+        codecs.party = party_dataframe
+        codecs.sub_office_type = sub_office_type_dataframe
+        assert codecs.decode_any_id('gender_id', 3, default_value='Not Specified') == 'Not Specified'
+    
+    def test_decode_any_id_with_to_name(self, gender_dataframe, office_type_dataframe, party_dataframe, sub_office_type_dataframe):
+        codecs = Codecs()
+        codecs.gender = gender_dataframe
+        codecs.office_type = office_type_dataframe
+        codecs.party = party_dataframe
+        codecs.sub_office_type = sub_office_type_dataframe
+        assert codecs.decode_any_id('gender_id', 10, to_name='gender_abbrev') == 'M'
+        assert codecs.decode_any_id('gender_id', 20, to_name='gender_abbrev') == 'F'
+    
 
-    # def test_decode_any_id_with_non_existing_value(self):
-    #     codecs = Codecs()
-    #     codecs.gender = pd.DataFrame({'gender_id': [1, 2], 'gender': ['Male', 'Female']})
-    #     assert codecs.decode_any_id('gender_id', 3) == 'unknown'
+    def test_decode_any_id_with_non_existing_from_name(self, gender_dataframe, office_type_dataframe, party_dataframe, sub_office_type_dataframe):
+        codecs = Codecs()
+        codecs.gender = gender_dataframe
+        codecs.office_type = office_type_dataframe
+        codecs.party = party_dataframe
+        codecs.sub_office_type = sub_office_type_dataframe
+        assert codecs.decode_any_id('non_existing_column', 1) == 'unknown'
+        
+    def test_decode_any_id_with_non_existing_to_name(self, gender_dataframe, office_type_dataframe, party_dataframe, sub_office_type_dataframe):
+        codecs = Codecs()
+        codecs.gender = gender_dataframe
+        codecs.office_type = office_type_dataframe
+        codecs.party = party_dataframe
+        codecs.sub_office_type = sub_office_type_dataframe
+        assert codecs.decode_any_id('gender_id', 1, to_name='non_existing_column') == 'unknown'
+        
+    def test_decode_any_id_with_non_existing_from_name_and_to_name(self, gender_dataframe, office_type_dataframe, party_dataframe, sub_office_type_dataframe):
+        codecs = Codecs()
+        codecs.gender = gender_dataframe
+        codecs.office_type = office_type_dataframe
+        codecs.party = party_dataframe
+        codecs.sub_office_type = sub_office_type_dataframe
+        assert codecs.decode_any_id('non_existing_column', 1, to_name='non_existing_column') == 'unknown'
+        
+    def test_decode_any_id_with_non_existing_from_name_and_to_name_and_default(self, gender_dataframe, office_type_dataframe, party_dataframe, sub_office_type_dataframe):
+        codecs = Codecs()
+        codecs.gender = gender_dataframe
+        codecs.office_type = office_type_dataframe
+        codecs.party = party_dataframe
+        codecs.sub_office_type = sub_office_type_dataframe
+        assert codecs.decode_any_id('non_existing_column', 1, to_name='non_existing_column', default_value='Not Specified') == 'Not Specified'
+        
 
-    # def test_decode_any_id_with_custom_default_value(self):
-    #     codecs = Codecs()
-    #     codecs.gender = pd.DataFrame({'gender_id': [1, 2], 'gender': ['Male', 'Female']})
-    #     assert codecs.decode_any_id('gender_id', 3, default_value='Not Specified') == 'Not Specified'
+    def test_codecs_decoders(self, gender_dataframe, office_type_dataframe, party_dataframe, sub_office_type_dataframe):
+        codecs = Codecs()
+        codecs.gender = gender_dataframe
+        codecs.office_type = office_type_dataframe
+        codecs.party = party_dataframe
+        codecs.sub_office_type = sub_office_type_dataframe
 
-    # def test_decode_any_id_with_to_name(self):
-    #     codecs = Codecs()
-    #     codecs.gender = pd.DataFrame({'gender_id': [1, 2], 'gender': ['Male', 'Female'], 'gender_abbrev': ['M', 'F']})
-    #     assert codecs.decode_any_id('gender_id', 1, to_name='gender_abbrev') == 'M'
-    #     assert codecs.decode_any_id('gender_id', 2, to_name='gender_abbrev') == 'F'
+        decoders = codecs.decoders
+        assert len(decoders) > 0
+        assert all(codec.type == 'decode' for codec in decoders)
 
-    # def test_decode_any_id_with_non_existing_from_name(self):
-    #     codecs = Codecs()
-    #     codecs.gender = pd.DataFrame({'gender_id': [1, 2], 'gender': ['Male', 'Female']})
-    #     assert codecs.decode_any_id('non_existing_column', 1) == 'unknown'
+    def test_codecs_encoders(self, gender_dataframe, office_type_dataframe, party_dataframe, sub_office_type_dataframe):
+        codecs = Codecs()
+        codecs.gender = gender_dataframe
+        codecs.office_type = office_type_dataframe
+        codecs.party = party_dataframe
+        codecs.sub_office_type = sub_office_type_dataframe
+
+        encoders = codecs.encoders
+        assert len(encoders) > 0
+        assert all(codec.type == 'encode' for codec in encoders)
+
+    def test_codecs_apply_codec(self, gender_dataframe, office_type_dataframe, party_dataframe, sub_office_type_dataframe):
+        codecs = Codecs()
+        codecs.gender = gender_dataframe
+        codecs.office_type = office_type_dataframe
+        codecs.party = party_dataframe
+        codecs.sub_office_type = sub_office_type_dataframe
+
+        df = pd.DataFrame({'gender_id': [10, 20], 'office_type_id': [10, 20], 'party_id': [100, 200], 'sub_office_type_id': [10, 20]})
+        result = codecs.apply_codec(df, codecs.decoders)
+        assert 'gender' in result.columns
+        assert 'office_type' in result.columns
+        assert 'party_abbrev' in result.columns
+        assert 'sub_office_type' in result.columns
+
+    def test_codecs_decode(self, gender_dataframe, office_type_dataframe, party_dataframe, sub_office_type_dataframe):
+        codecs = Codecs()
+        codecs.gender = gender_dataframe
+        codecs.office_type = office_type_dataframe
+        codecs.party = party_dataframe
+        codecs.sub_office_type = sub_office_type_dataframe
+
+        df = pd.DataFrame({'gender_id': [10, 20], 'office_type_id': [10, 20], 'party_id': [100, 200], 'sub_office_type_id': [10, 20]})
+        result = codecs.decode(df)
+        assert 'gender' in result.columns
+        assert 'office_type' in result.columns
+        assert 'party_abbrev' in result.columns
+        assert 'sub_office_type' in result.columns
+
+    def test_codecs_encode(self, gender_dataframe, office_type_dataframe, party_dataframe, sub_office_type_dataframe):
+        codecs = Codecs()
+        codecs.gender = gender_dataframe
+        codecs.office_type = office_type_dataframe
+        codecs.party = party_dataframe
+        codecs.sub_office_type = sub_office_type_dataframe
+
+        df = pd.DataFrame({'gender': ['Male', 'Female'], 'office_type': ['Office A', 'Office B'], 'party_abbrev': ['PA', 'PB'], 'sub_office_type': ['Description A', 'Description B']})
+        result = codecs.encode(df)
+        assert 'gender_id' in result.columns
+        assert 'office_type_id' in result.columns
+        
+        # FIXME: party encoder is using self.party_abbrev2id instead of self.party2id. See #152.
+        assert 'party_id' not in result.columns
+        assert 'party_abbrev' in result.columns
+        
+        assert 'sub_office_type_id' in result.columns
+
+    def test_codecs_is_decoded(self, gender_dataframe, office_type_dataframe, party_dataframe, sub_office_type_dataframe):
+        codecs = Codecs()
+        codecs.gender = gender_dataframe
+        codecs.office_type = office_type_dataframe
+        codecs.party = party_dataframe
+        codecs.sub_office_type = sub_office_type_dataframe
+
+        df = pd.DataFrame({'gender_id': [10, 20], 'office_type_id': [10, 20], 'party_id': [100, 200], 'sub_office_type_id': [10, 20]})
+        assert not codecs.is_decoded(df)
+        df = codecs.decode(df)
+        assert codecs.is_decoded(df)
 
 class TestPersonCodecs:
 
@@ -333,3 +446,5 @@ class TestPersonCodecs:
         })
         assert person_codecs.any2any('pid', 'person_id') == {1: 'p1', 2: 'p2'}
         assert person_codecs.any2any('person_id', 'name') == {'p1': 'John Doe', 'p2': 'Jane Doe'}
+
+
