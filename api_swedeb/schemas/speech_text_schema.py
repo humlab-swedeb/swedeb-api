aed336067ce0ec3e7d435a93eaf2ea282203cde5
roger-mahler <roger.mahler@hotmail.com>
2025-05-28 12:06:26 +0200

    change: add page_number to SpeechesTextResultItem change: use get_speech when populating SpeechesTextResultItem instance

--- api_swedeb/schemas/speech_text_schema.py
+++ api_swedeb/schemas/speech_text_schema.py
@@ -4,3 +4,4 @@ from pydantic import BaseModel, Field
 class SpeechesTextResultItem(BaseModel):
     speaker_note: str = Field(None, description="Speaker note")
     speech_text: str = Field(None, description="Speech text")
+    page_number: int = Field(None, description="Page number")
